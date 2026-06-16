bf877b2766cfbdd6972a3319db9d6a5e433ca615
Arkaprabha Das <arkaprabha31@gmail.com>
2026-06-17 00:50:51 +0530

    bug fixes

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -3,7 +3,18 @@ import Comment from "../models/Comments.js"
 export const editComment = async (req, res)=>{
     try{
         const commentId = req.params.commentId;
+        const existingComment = await Comment.findById(commentId);
+        if(existingComment.userId.toString() !== req.user.userId)
+        {
+            return res.status(400).json({
+                message: "User Not Authenticated!"
+            })
+        }
         const updatedComment = await Comment.findByIdAndUpdate(commentId, {$set:{content: req.body.content}}, {new: true});
+        if(!updatedComment)
+        {
+            return res.status(400).json({message: "The comment doesnt exist!"});
+        }
         res.status(200).json(updatedComment);
     }catch(err)
     {
@@ -15,6 +26,10 @@ export const deleteComment = async (req, res) =>{
     try{
         const commentId = req.params.commentId
         const deletedComment = await Comment.findByIdAndDelete(commentId);
+        if(!deleteComment)
+        {
+            return res.status(400).json({message: "Comment not found!"})
+        }
         res.status(200).json({
             message: "Comment deleted successfully!"
         })
