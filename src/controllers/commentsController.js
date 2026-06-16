4af5bac40b75cab8edb3fb8c2e445f527e64a711
Arkaprabha Das <arkaprabha31@gmail.com>
2026-06-17 01:06:45 +0530

    bug resolved for authentication of user in editComment

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -6,9 +6,9 @@ export const editComment = async (req, res)=>{
         const existingComment = await Comment.findById(commentId);
         if(!existingComment)
         {
-            return res.status(400).json({message: "The comment doesnt exist!"});
+            return res.status(403).json({message: "The comment doesnt exist!"});
         }
-        if(existingComment.userId.equals(req.user.userId))      // used equals instead of == and converting id to string
+        if(!existingComment.userId.equals(req.user.userId))      // used equals instead of == and converting id to string
         {
             return res.status(401).json({
                 message: "User Not Authenticated!"
@@ -17,7 +17,7 @@ export const editComment = async (req, res)=>{
         const updatedComment = await Comment.findByIdAndUpdate(commentId, {$set:{content: req.body.content}}, {new: true});
         if(!updatedComment)
         {
-            return res.status(400).json({message: "The comment doesnt exist!"});
+            return res.status(404).json({message: "The comment doesnt exist!"});
         }
         res.status(200).json(updatedComment);
     }catch(err)
@@ -32,7 +32,7 @@ export const deleteComment = async (req, res) =>{
         const existingComment = await Comment.findById(commentId);
         if(!existingComment)
         {
-            return res.status(400).json({message: "The comment doesnt exist!"});
+            return res.status(403).json({message: "The comment doesn't exist!"});
         }
         if(existingComment.userId.toString() !== req.user.userId)
         {
