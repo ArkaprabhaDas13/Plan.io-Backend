6d27b455a3fa1c21c67938e9d88e7c0f0eb83dac
Arkaprabha Das <arkaprabha31@gmail.com>
2026-08-26 18:05:32 +0530

    Created a Test route for testing GET /

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -1,21 +1,9 @@
-import express from 'express';
-const router = express.Router();
-import User from '../models/Users.js'
-import Task from '../models/Tasks.js'
+import express from 'express'
 
-// test User route
-router.get("/test-user", async(req, res)=>{
-    res.send("User Route!");
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    res.send('Server OK')
 })
 
-// test Tasks route
-router.get("/test-tasks", async(req, res)=>{
-    res.send("Test route!");
-})
-
-// test Audit route
-router.get("/test-project", async(req, res)=>{
-    res.send("Test Project!");
-})
-
-export default router;
+export default router
